13669213f00fcab9151fc20f382cf0b37427ddbd
Gaetan <gaetan.mounsamy@efrei.net>
2022-09-25 16:40:14 +0200

    Ex 4: create HomePage component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
+<template>
 
+    <HomePage />
+
+</template>
 
 <script>
-
-
+import HomePage from "@/pages/HomePage";
 export default {
   name: 'App',
   components: {
+    HomePage
 
   }
 }
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="text-align: center">
+    <p>Youpi ma premiere page en Vue.</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomePage',
+
+}
+</script>
