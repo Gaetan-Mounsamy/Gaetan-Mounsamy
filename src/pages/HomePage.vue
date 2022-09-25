651afd4c2ded3ffda3064ec52849814f8587b2db
Gaetan <gaetan.mounsamy@efrei.net>
2022-09-25 21:58:00 +0200

    Ex 9: add AsyncButton

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,29 +5,43 @@
     <base-button>
       <p>BaseButton</p>
     </base-button>
+
     <br>
     <base-button disabled>
       <p>BaseButton disabled</p>
     </base-button>
+
     <br>
     <base-button color="warn">
       <p>BaseButton colors props</p>
     </base-button>
+
     <br>
     <base-button color="danger">
       <p>BaseButton colors props v2</p>
     </base-button>
 
+    <br>
+    <async-button @click="wait">
+      desactive x seconde à chaque click
+    </async-button>
+
   </div>
 </template>
 
 <script>
-
+  import AsyncButton from '../components/AsyncButton.vue'
   import BaseButton from "@/components/BaseButton";
 
   export default
   {
     name: 'HomePage',
-    components: {BaseButton},
+    components: {BaseButton, AsyncButton},
+    methods: {
+      wait ()
+      {
+        return new Promise(resolve => setTimeout(resolve, 2000))
+      }
+    }
   }
 </script>
